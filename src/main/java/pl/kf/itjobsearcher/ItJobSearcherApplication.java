f00524ea2d16a4099293ec67fde742e8a8a505c3
KF <kubafurtacz@gmail.com>
2023-07-25 19:50:35 +0200

    initial setup

---
+++ src/main/java/pl/kf/itjobsearcher/ItJobSearcherApplication.java
@@ -0,0 +1,13 @@
+package pl.kf.itjobsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItJobSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItJobSearcherApplication.class, args);
+	}
+
+}

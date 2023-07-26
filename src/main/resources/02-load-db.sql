361d85c6720dce28fb0e66ab40ce5c040142d0cd
KF <kubafurtacz@gmail.com>
2023-07-26 19:08:39 +0200

    Added docker files, sql files and env

---
+++ src/main/resources/02-load-db.sql
@@ -0,0 +1,4 @@
+--LOAD DATA
+INSERT INTO jj (company, contract, money, exp_lvl, req_exp, techs) VALUES('Aab','B2B','5000','junior','0','java, kotlin, sql'),
+                                                                         ('Bcc', 'UoP', '10000','senior','5','java, kubernetees,docker,sql'),
+                                                                         ('Cdd','UoP','25000','Architekt','10','java, aws, kotlin, sql, python');

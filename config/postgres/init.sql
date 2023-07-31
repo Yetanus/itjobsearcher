ec9db215fdf18518491cdb07b6ec8b5a71499e88
Damian Gadecki <damian.gadecki@vm.pl>
2023-07-31 15:26:28 +0200

    feature:NO-ISSUE Create local instance for PostgreSQL

---
+++ config/postgres/init.sql
@@ -0,0 +1,3 @@
+CREATE USER it_job_searcher_connect WITH ENCRYPTED PASSWORD 'it_job_searcher_connect';
+CREATE DATABASE it_job_searcher;
+GRANT ALL PRIVILEGES ON DATABASE it_job_searcher TO it_job_searcher_connect;

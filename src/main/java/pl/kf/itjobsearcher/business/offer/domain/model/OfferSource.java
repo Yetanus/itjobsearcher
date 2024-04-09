034da016ebd5f1ed7960689a64797a73f589ca05
KF <kubafurtacz@gmail.com>
2024-04-09 13:06:34 +0200

    [No-Issue] Converter rebuild

--- src/main/java/pl/kf/itjobsearcher/business/offer/domain/model/OfferSource.java
+++ src/main/java/pl/kf/itjobsearcher/business/offer/domain/model/OfferSource.java
@@ -3,5 +3,6 @@ package pl.kf.itjobsearcher.business.offer.domain.model;
 public enum OfferSource {
     JUST_JOIN,
     NO_FLUFF_JOBS,
-    LINKEDIN
+    LINKEDIN,
+    JANUSZ_SOFT
 }
---
+++ src/main/java/pl/kf/itjobsearcher/business/offer/domain/service/converter/OfferConverter.java
@@ -0,0 +1,9 @@
+package pl.kf.itjobsearcher.business.offer.domain.service.converter;
+
+import pl.kf.itjobsearcher.business.offer.dto.CreateOfferCommand;
+
+import java.util.List;
+
+public interface OfferConverter<T> {
+    List<CreateOfferCommand> convert(T source);
+}
